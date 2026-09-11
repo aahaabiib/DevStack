63ad060942cafbe634546771057eb61a908a80e5
aahaabiib@gmail.com <aahaabiib@gmail.com>
2026-09-12 01:30:36 +0600

    Vite, React,Tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function  App() {
+  
+  return (
+
+    <h1 className="text-4xl font-bold text-center mt-10 text-pink-600">
+      DevStack Setup Ok
+    </h1>
+  )
+  
+}
+
+export default App
