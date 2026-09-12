584a2e3f322b66c1d11e4ce795b65ced54f66088
aahaabiib@gmail.com <aahaabiib@gmail.com>
2026-09-12 07:20:13 +0600

    Successfully loaded technology data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from "react"
+import technologiesData from "./data/technologies.json"
 
 
 function  App() {
+  const [technologies, setTechnologies] = useState([])
+  const [loading, setLoading] = useState(true)
   
+  useEffect(() => {
+    
+      setTechnologies(technologiesData)
+      setLoading(false)
+     }, [])
+
+  if(loading){
+    return <p className="text-center mt-10">Loading...</p>
+  }
+
+
   return (
 
-    <h1 className="text-4xl font-bold text-center mt-10 text-pink-600">
-      DevStack Setup Ok
-    </h1>
+    <div>
+      
+      <h1 className="text-3xl font-bold text-center mt-6">
+        Total Techologies: {technologies.length}
+      </h1>
+      <ul className="max-w-md mx-auto mt-4">
+        {technologies.map((tech) => (
+          <li key={tech.id} className="border-b py-2">
+            {tech.name} - {tech.category}
+          </li>
+        ))}
+      </ul>
+    </div>
+
   )
-  
+
 }
 
 export default App
---
+++ src/components/Grid.jsx
@@ -0,0 +1,10 @@
+
+
+
+
+function Grid(){
+    return <div>Grid</div>
+}
+
+
+export default Grid 
