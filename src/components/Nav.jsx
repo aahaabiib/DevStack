6a3f8a7328117706296ca8a1ecdfb5e7f80efffa
aahaabiib@gmail.com <aahaabiib@gmail.com>
2026-09-13 21:53:54 +0600

    Fix mobile hamburger menu classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,24 @@
-
-
+import { useState } from "react"
+import {Menu, X} from 'lucide-react'
 
 function Navbar(){
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <nav className="sticky top-0 z-50 bg-white shadow-md">
             <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+                
+                 {/* Hamburger - mobile only */}
+                <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+                    {menuOpen ? <X size={25}/> : <Menu size={25}/>}
+                </button>
+
+
                 <div>
                     <img src="/logo-text.png" alt="DevStack-logo"  />
                 </div>
 
-                <ul className="hedden md:flex gap-8 font-medium text-gray-700">
+                <ul className="hidden md:flex gap-8 font-medium text-gray-700">
                     <li className="hover:text-pink-600 cursor-pointer">Home</li>
                     <li className="hover:text-pink-600 cursor-pointer">Technologies</li>
                     <li className="hover:text-pink-600 cursor-pointer">Projects</li>
@@ -22,6 +31,17 @@ function Navbar(){
                     <button className="px-5 py-2 rounded-full text-white font-medium bg-gradient-to-r from-orange-500 via-pink-500 to-violet-600">Sign Up</button>
                 </div>
             </div>
+
+            {/* Mobile dropdown menu  */}
+            {menuOpen && (
+                <ul className="md:hidden flex flex-col gap-4 px-6 pb-4 font-medium text-gray-700">
+                    <li className="hover:text-pink-600 cursor-pointer">Home</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Technologies</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Projects</li>
+                    <li className="hover:text-pink-600 cursor-pointer">About</li>
+                    <li className="hover:text-pink-600 cursor-pointer">Contact</li>
+                </ul>
+            )}
         </nav>
     )
 }
